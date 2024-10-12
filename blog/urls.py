56990858edb4f58d576135eb63e74d615e96c586
Doctor-Bonov <nagirnyj_vv@fizmat.tnpu.edu.ua>
2024-10-12 11:17:26 +0300

    Allahu Akbar

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import  re_path
 from . import views
 urlpatterns = [
     re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
